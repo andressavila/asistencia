7d31f2aa5991a7fbdca8cbed1732afff2f3edf56
andressavila <davidmontenegro-b@hotmail.com>
2024-09-02 12:58:51 -0500

    e

--- src/app/components/tablas/tabla-empleado.component.ts
+++ src/app/components/tablas/tabla-empleado.component.ts
@@ -3,18 +3,20 @@ import { Component, OnInit } from '@angular/core';
 
 
 export interface PeriodicElement {
+  // interface de datos de la tabla
   name: string;
   position: number;
   weight: number;
   symbol: string;
+  horas : number;
 }
-
+// data de nuestra table de empleabilidad de control de datos de entrada
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 1, name: 'raul perez mesarifher', weight: 0.9022024 , symbol: 'aipa', horas:1255},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', horas:1255},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', horas:1255},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', horas:1255},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', horas:12.55},
 ];
 
 
@@ -29,7 +31,7 @@ export class tableComponent implements OnInit {
 
   ngOnInit() { }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight','horas','symbol'];
   dataSource = ELEMENT_DATA;
 
 }
