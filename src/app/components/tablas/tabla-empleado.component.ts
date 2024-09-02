66e1a57a9f6a94505af3e0f05ba3439a95c4f3ff
andressavila <davidmontenegro-b@hotmail.com>
2024-09-02 13:07:02 -0500

    r

--- src/app/components/tablas/tabla-empleado.component.ts
+++ src/app/components/tablas/tabla-empleado.component.ts
@@ -6,17 +6,17 @@ export interface PeriodicElement {
   // interface de datos de la tabla
   name: string;
   position: number;
-  weight: number;
+  fecha: number;
   symbol: string;
   horas : number;
 }
 // data de nuestra table de empleabilidad de control de datos de entrada
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'raul perez mesarifher', weight: 0.9022024 , symbol: 'aipa', horas:1255},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', horas:1255},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', horas:1255},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', horas:1255},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', horas:12.55},
+  {position: 1, name: 'nombre del empleado', fecha: 0.9022024 , symbol: 'aipa', horas:1255},
+  {position: 2, name: 'nombre del empleado', fecha: 4.0026, symbol: 'He', horas:1255},
+  {position: 3, name: 'nombre del empleado', fecha: 6.941, symbol: 'Li', horas:1255},
+  {position: 4, name: 'nombre del empleado', fecha: 9.0122, symbol: 'Be', horas:1255},
+  {position: 5, name: 'nombre del empleado', fecha: 10.811, symbol: 'B', horas:12.55},
 ];
 
 
@@ -31,7 +31,7 @@ export class tableComponent implements OnInit {
 
   ngOnInit() { }
 
-  displayedColumns: string[] = ['position', 'name', 'weight','horas','symbol'];
+  displayedColumns: string[] = ['position', 'name', 'fecha','horas','symbol'];
   dataSource = ELEMENT_DATA;
 
 }
