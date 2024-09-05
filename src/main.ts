ecfc085e70d1acb1458bb23fab58147315bbea08
andressavila <davidmontenegro-b@hotmail.com>
2024-09-05 17:25:18 -0500

    f

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+
+
+
+
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
